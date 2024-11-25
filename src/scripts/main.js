e34243672337f129b48f1dbc3ae4ab8a6d694b80
Raimundo <raimundo_nn16@hotmail.com>
2024-11-24 22:31:20 -0300

    finalizando projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 15:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOVento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOVento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOVento}h ${minutosAteOVento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `<p style='color: red'> Evento já realizado! </p>`;
+    }
+
+}, 1000);
+
+
